f5aa9094e885c07ff7a4ad520652f7ce002747cd
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2023-08-25 16:35:23 +0530

    fixed date issue

--- application/config/constants.php
+++ application/config/constants.php
@@ -26,7 +26,10 @@ defined('ADDRESS') or define('ADDRESS', "117 Windwoods Drive, Collegeville PA 19
 
 defined('D_DATE') or define('D_DATE', "2023-06-15");
 defined('R_DATE') or define('R_DATE', "2023-07-30");
-defined('DR_DATE') or define('DR_DATE', "Jun 15 - Jul 30");
+
+define('OfferDateRange', date('d M', strtotime("$currentYear-$currentMonth-" . ($middleDay + 1))) . " - " . date('d M', strtotime("$currentYear-$currentMonth-" . cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear))));
+
+defined('DR_DATE') or define('DR_DATE', OfferDateRange);
 
 
 
